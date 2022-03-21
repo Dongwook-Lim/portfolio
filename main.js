f3696a332dd8390ddd905b07c7fe18424c09c2de
Dongwook Lim <ldu9905@gmail.com>
2022-03-21 18:52:49 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -6,11 +6,30 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navbar height: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView();
+}
+
+const navbarMenu = document.querySelector('.navbar__menu');
+
+navbarMenu.addEventListener('click', (event) => {
+  const dataset = event.target.dataset;
+  const link = dataset.link;
+  console.log(link);
+
+  if (link == null) {
+    return;
+  }
+
+  scrollIntoView(link);
 });
