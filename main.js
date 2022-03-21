b04f2d9c480d5b8da3f63b9dcbe7fd0583e440b0
Dongwook Lim <ldu9905@gmail.com>
2022-03-21 20:51:02 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -15,11 +15,6 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView();
-}
-
 const navbarMenu = document.querySelector('.navbar__menu');
 
 navbarMenu.addEventListener('click', (event) => {
@@ -56,6 +51,28 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  const scrolledHeight = window.scrollY;
-  home.style.opacity = 1 - scrolledHeight / homeHeight;
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Show "arrow up" button when scrolling up
+
+const arrowUpBtn = document.querySelector('.arrow-up-btn');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUpBtn.classList.add('visible');
+  } else {
+    arrowUpBtn.classList.remove('visible');
+  }
+});
+
+// Handle click on the "arrow up" button
+
+arrowUpBtn.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView();
+}
