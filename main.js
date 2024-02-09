7ae4f0ce71bbf4f669bd72fc725efa751952a7f3
Dongwook Lim <ldu9905@gmail.com>
2024-02-09 15:44:22 +1100

    Add some animations

--- main.js
+++ main.js
@@ -1,20 +1,57 @@
 'use strict';
 
 // Make navbar transparent when in is on the top
+// const navbar = document.querySelector('#navbar');
+// const navbarHeight = navbar.getBoundingClientRect().height;
 
-const navbar = document.querySelector('#navbar');
-const navbarHeight = navbar.getBoundingClientRect().height;
+// document.addEventListener('scroll', () => {
+//   if (window.scrollY > navbarHeight) {
+//     navbar.classList.add('navbar--dark');
+//   } else {
+//     navbar.classList.remove('navbar--dark');
+//   }
+// });
+
+// Typing Animation
+var typed = new Typed('.home__title', {
+  strings: ["I'm Leon Lim ✨", 'A Front-end Web Developer ⭐️'],
+  typeSpeed: 70,
+  backSpeed: 40,
+  backDelay: 1000,
+  loop: true,
+});
+
+// Make home slowly fade to transparent when the window scrolls down
+// Make tech stack icons disappear and appear
+const nav = document.querySelector('#navbar');
+const navbarHeight = nav.offsetHeight;
+
+const homeContainer = document.querySelector('.home__container');
+const homeContainerHeight = homeContainer.offsetHeight;
+const homeContainerTop = homeContainer.offsetTop;
+
+const homeTechStack = document.querySelector('.home__tech-stack');
+const techStackIcons = document.querySelector('.tech-stack-icons');
 
 document.addEventListener('scroll', () => {
-  if (window.scrollY > navbarHeight) {
-    navbar.classList.add('navbar--dark');
+  const scroll = window.scrollY;
+  homeContainer.style.opacity = 1 - scroll / homeContainerHeight;
+  if (scroll >= homeContainerHeight + homeContainerTop - navbarHeight) {
+    homeTechStack.style.transform = 'translateY(50px)';
+    homeTechStack.style.opacity = 0;
+
+    techStackIcons.style.transform = 'translateX(0px)';
+    techStackIcons.style.opacity = 1;
   } else {
-    navbar.classList.remove('navbar--dark');
+    homeTechStack.style.transform = 'translateY(0px)';
+    homeTechStack.style.opacity = 1;
+
+    techStackIcons.style.transform = 'translateX(-50px)';
+    techStackIcons.style.opacity = 0;
   }
 });
 
 // Handle scrolling when tapping on the navbar menu
-
 const navbarMenu = document.querySelector('.navbar__menu');
 
 navbarMenu.addEventListener('click', (event) => {
@@ -32,44 +69,24 @@ navbarMenu.addEventListener('click', (event) => {
 });
 
 // Navbar toggle button for small screen
-
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 
 navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
 
-// Handle click on "contact me" button on home
-
-// const homeContactBtn = document.querySelector('.home__contact');
-
-// homeContactBtn.addEventListener('click', () => {
-//   scrollIntoView('contact');
-// });
-
 // Handle click on logo on navbar
-
 const navbarLogo = document.querySelector('.navbar__logo');
 
 navbarLogo.addEventListener('click', () => {
   scrollIntoView('home');
 });
 
-// Make home slowly fade to transparent when the window scrolls down
-
-const home = document.querySelector('.home__container');
-const homeHeight = home.getBoundingClientRect().height;
-
-document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / homeHeight;
-});
-
 // Show "arrow up" button when scrolling up
-
 const arrowUpBtn = document.querySelector('.arrow-up-btn');
 
 document.addEventListener('scroll', () => {
-  if (window.scrollY > homeHeight / 2) {
+  if (window.scrollY > homeContainerHeight / 2) {
     arrowUpBtn.classList.add('visible');
   } else {
     arrowUpBtn.classList.remove('visible');
@@ -77,51 +94,47 @@ document.addEventListener('scroll', () => {
 });
 
 // Handle click on the "arrow up" button
-
 arrowUpBtn.addEventListener('click', () => {
   scrollIntoView('home');
 });
 
-// Projects
+// // Projects
+// const categoryBtnContainer = document.querySelector('.work__categories');
+// const projectContainer = document.querySelector('.work__projects');
+// const projects = document.querySelectorAll('.project');
 
-const categoryBtnContainer = document.querySelector('.work__categories');
-const projectContainer = document.querySelector('.work__projects');
-const projects = document.querySelectorAll('.project');
+// categoryBtnContainer.addEventListener('click', (event) => {
+//   const filter =
+//     event.target.dataset.filter || event.target.parentNode.dataset.filter;
+//   if (filter == null) {
+//     return;
+//   }
 
-categoryBtnContainer.addEventListener('click', (event) => {
-  const filter =
-    event.target.dataset.filter || event.target.parentNode.dataset.filter;
-  if (filter == null) {
-    return;
-  }
+//   projectContainer.classList.add('anim-out');
 
-  projectContainer.classList.add('anim-out');
+//   setTimeout(() => {
+//     projectContainer.classList.remove('anim-out');
+//     projects.forEach((project) => {
+//       if (filter === 'all' || filter === project.dataset.type) {
+//         project.classList.remove('invisible');
+//       } else {
+//         project.classList.add('invisible');
+//       }
+//     });
+//   }, 300);
 
-  setTimeout(() => {
-    projectContainer.classList.remove('anim-out');
-    projects.forEach((project) => {
-      if (filter === 'all' || filter === project.dataset.type) {
-        project.classList.remove('invisible');
-      } else {
-        project.classList.add('invisible');
-      }
-    });
-  }, 300);
-
-  // Remove selection from the previous item and select the new one
-
-  const active = document.querySelector('.category__btn.selected');
-  active.classList.remove('selected');
-  const target =
-    event.target.nodeName === 'BUTTON' ? event.target : event.target.parentNode;
-  target.classList.add('selected');
-});
+//   // Remove selection from the previous item and select the new one
+//   const active = document.querySelector('.category__btn.selected');
+//   active.classList.remove('selected');
+//   const target =
+//     event.target.nodeName === 'BUTTON' ? event.target : event.target.parentNode;
+//   target.classList.add('selected');
+// });
 
 // Active selected menu item when scrolled to specific section
 // 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
 // 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
 // 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
-
 const sections = document.querySelectorAll('section');
 const menuItems = document.querySelectorAll('.navbar__menu__item');
 
